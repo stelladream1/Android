1c67b2689be215e0e481b0339131aaf2e136dc6e
stelladream1 <kimsohee1206@naver.com>
2021-07-11 21:53:42 +0900

    backgroundtask

---
+++ BackgroundTask/app/src/main/java/com/example/backgroundtask/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.backgroundtask
+
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.IBinder
+import android.view.Menu
+import android.view.MenuItem
+import com.example.backgroundtask.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var scope: CoroutineScope
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.buttonStartCo.setOnClickListener {
+            startCoroutine()
+
+        }
+
+        binding.buttonStopCo.setOnClickListener {
+            stopCoroutine()
+        }
+
+        binding.buttonStartService.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // Android 8.0
+                    startForegroundService(it) // must call startForground in a few seconds.
+                } else {
+                    startService(it)
+                }
+            }
+        }
+
+        binding.buttonStartedCount.setOnClickListener {
+            binding.textViewCount.text = "${myService?.startedCount}"
+        }
+
+    }
+
+    private fun startCoroutine() {
+        scope = CoroutineScope(Dispatchers.Default + Job()).apply {
+            launch {
+                for (i in 1..10) {
+                    delay(1000)
+                    withContext(Dispatchers.Main) {
+                        binding.textView.text = "$i"
+                    }
+                }
+            }
+
+            launch {
+                for (i in 1..10) {
+                    delay(1000)
+                    println("#$i")
+                }
+            }
+        }
+    }
+
+    private fun stopCoroutine() = scope.cancel()
+
+    private var myService: MyService? = null
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            myService = (service as MyService.LocalBinder).getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            myService = null
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, MyService::class.java).also {
+            bindService(it, serviceConnection, BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(serviceConnection)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.broadcastTest) {
+            startActivity(Intent(this, BroadcastActivity::class.java))
+            return true
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
+    }
+}
